138fef11de0c44684ef020d87b1c251cab21590e
nerayn4 <hei.nyharena@gmail.com>
2025-09-04 08:12:14 +0300

    first commit

---
+++ maven/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $nyharena;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
